3bc91c499c78bdf01477c765c910f66152b6ba6b
sureshatt <suresh.attanayake@gmail.com>
2017-07-16 11:23:25 +0200

    comming my_first_api_project-static_names_list

---
+++ My_first_api_project/Controllers/NamesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_first_api_proj.Controllers
+{
+    [Route("api/[controller]")]
+    public class NamesController : Controller
+    {
+        List<string> names = new List<string>(){ "Alice", "Bob", "Eve", "Mallory", "Trent"};
+        // GET api/names
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return names;
+        }
+
+        // GET api/names/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return names.ElementAt(id);
+        }
+
+        // POST api/names
+        [HttpPost]
+        public IEnumerable<string> Post([FromBody]string name)
+        {
+            names.Add(name);
+            return names;
+        }
+
+        // PUT api/names/5
+        [HttpPut("{id}")]
+        public IEnumerable<string> Put(int id, [FromBody]string name)
+        {
+            names.Insert(id, name);
+            return names;
+        }
+
+        // DELETE api/names/5
+        [HttpDelete("{id}")]
+        public IEnumerable<string> Delete(int id)
+        {
+            names.RemoveAt(id);
+            return names;
+        }
+    }
+}

362c45f9b1a850a7fcddb76812abbc1239e48e08
sureshatt <suresh.attanayake@gmail.com>
2017-07-16 13:14:58 +0200

    adding static names project

---
+++ static_names_list_api_project/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace static_names_list_api_project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:65387")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
